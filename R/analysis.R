278a849f23beabcf234131f5514f0f7e30e65093
Ramiro Magno <ramiro.magno@gmail.com>
2021-10-30 13:28:15 +0100

    Refactor of `get_analyses()` to use `request_parallel()`

--- R/analysis.R
+++ R/analysis.R
@@ -74,16 +74,23 @@ get_analyses <- function(species_name,
 
   # Usually we'd use purrr::map here but we opted for plyr::llply
   # for a no frills alternative with progress bar support.
-  progress <- dplyr::if_else(progress_bar && interactive(), 'text', 'none')
-  responses <- plyr::llply(
-    .data = resource_urls,
-    .fun = request,
-    verbose = verbose,
-    warnings = warnings,
-    .progress = progress)
+  # progress <- dplyr::if_else(progress_bar && interactive(), 'text', 'none')
+  # responses <- plyr::llply(
+  #   .data = resource_urls,
+  #   .fun = request,
+  #   verbose = verbose,
+  #   warnings = warnings,
+  #   .progress = progress)
+  responses <-
+    request_parallel(
+      resource_urls,
+      verbose = verbose,
+      warnings = warnings,
+      progress_bar = progress_bar
+    )
 
   # Only keep those responses that responded successfully, i.e. with status == "OK".
-  responses_ok <- purrr::keep(responses, ~ .x$status == 'OK')
+  responses_ok <- purrr::keep(responses, ~ identical(.x$status, 'OK'))
 
   # If none of the responses were successful then return an empty linkage
   # disequilibrium tibble.
