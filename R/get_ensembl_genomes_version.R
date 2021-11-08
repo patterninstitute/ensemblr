8f7e8879df6f3122820befbe98c3f5af321e89f8
Ramiro Magno <ramiro.magno@gmail.com>
2021-11-08 00:01:46 +0000

    Added `get_ensembl_genomes_version()` function

---
+++ R/get_ensembl_genomes_version.R
@@ -0,0 +1,22 @@
+#' Get Ensembl Genomes version
+#'
+#' Returns the Ensembl Genomes version of the databases backing this service.
+#'
+#' @param verbose Whether to be chatty.
+#' @param warnings Whether to print warnings.
+#'
+#' @return An integer value: the Ensembl Genomes version.
+#'
+#' @examples
+#' get_ensembl_genomes_version()
+#'
+#' @export
+get_ensembl_genomes_version <- function(verbose = FALSE, warnings = TRUE) {
+
+  response <- request('/info/eg_version', verbose = verbose, warnings = warnings)
+
+  if(identical(response$status, 'OK'))
+    return(response$content$version)
+  else
+    return(NA_integer_)
+}
